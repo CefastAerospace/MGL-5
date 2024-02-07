db14aae38b47f051bd7ade340f1d6616f440ac86
CefastAerospace <49575357+CefastAerospace@users.noreply.github.com>
2024-02-07 14:15:40 -0300

    Update testeRecebe.ino

--- Programação/Teste Lora(Rádio)/LoRaReceiver/testeRecebe.ino
+++ Programação/Teste Lora(Rádio)/LoRaReceiver/testeRecebe.ino
@@ -1,25 +1,16 @@
 #include <SoftwareSerial.h>
- 
-#define LED1  4  
+#include <LoRa.h> 
  
 SoftwareSerial loraSerial(2, 3); // TX, RX
  
 void setup() {
-  pinMode(LED1, OUTPUT);
   Serial.begin(9600);
   loraSerial.begin(9600);  
 }
  
 void loop() { 
-  if(loraSerial.available() > 1){
-    String input = loraSerial.readString();
-    Serial.println(input);  
-    if(input == "on") {
-      digitalWrite(LED1, HIGH);  
-    } 
-    if(input == "off") {
-      digitalWrite(LED1, LOW);
+  while (loraSerial.available() > 1) {
+      Serial.print((char)LoRa.read());
     }
-  }
   delay(20);
 }
