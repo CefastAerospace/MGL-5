3fbe02999df9c86701363f0efcee4b9ea13df5be
Gabriel Araujo <gabrielaraujo143@gmail.com>
2024-10-13 15:52:24 -0300

    feat: adding DHT11 module

--- Programação/Satelite/Satelite.ino
+++ Programação/Satelite/Satelite.ino
@@ -1,6 +1,9 @@
 #include <SPI.h>
 #include <LoRa.h>
 #include <DHT.h>
+#include <Wire.h>
+#include <Adafruit_BMP280.h>
+
 
 #define BUZZER_PIN 25
 #define DHT11_PIN 19
@@ -9,6 +12,7 @@
 #define LORA_DIO0 14
 
 DHT dht11(DHT11_PIN, DHT11);
+Adafruit_BMP280 bmp;  //I2C
 
 unsigned long lastSendTime = 0;
 const long interval = 5000; // Intervalo de 2 segundos
@@ -30,10 +34,20 @@ void setup() {
   }
 
   Serial.println("LoRa Initializing OK!");
-}
+
+  if (!bmp.begin(0x76)) { //Definindo o endereço I2C como 0x76. Mudar, se necessário, para (0x77)/
+
+    //Imprime mensagem de erro no caso de endereço invalido ou não localizado. Modifique o valor
+    Serial.println(F(" Não foi possível encontrar um sensor BMP280 válido, verifique a fiação ou "
+                     "tente outro endereço!"));
+    while (1) delay(10);
+  }
+ }
+
 
 void loop() {
   // printDTH11();
+  // printBMP280()
 
   // Verificar se é hora de enviar um pacote
   if (millis() - lastSendTime > interval) {
@@ -92,4 +106,21 @@ void printDTH11(){
   Serial.print("\tTemperature: ");
   Serial.print(dht11.readTemperature());
   Serial.println("°C");
+}
+
+void printBMP280(){
+  //Imprimindo os valores de Temperatura
+  Serial.print(F("Temperatura = "));
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+
+  //Imprimindo os valores de Pressão
+  Serial.print(F("Pressão = "));
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+
+  //Imprimindo os valores de Altitude Aproximada
+  Serial.print(F("Altitude Aprox = "));
+  Serial.print(bmp.readAltitude(1013.25)); /* Ajustar a pressão de nível do mar de acordo com o local!*/
+  Serial.println(" m");
 }
